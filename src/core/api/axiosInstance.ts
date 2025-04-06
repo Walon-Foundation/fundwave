bf2e62ddc52e13af161d4a09dc240669795e7644
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-04-06 18:06:23 +0000

    edit axios

--- src/core/api/axiosInstance.ts
+++ src/core/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://fundwave-vert.vercel.app/api/',
+    baseURL: 'https://fundwavesl.vercel.app/api/',
     withCredentials: true,
 });
 
