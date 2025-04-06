94a1a504e210c85c471978c08482e71c00fbb147
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-04-06 18:13:03 +0000

    edit feautured campaign

--- src/components/heroPart/FeatureCampaign.tsx
+++ src/components/heroPart/FeatureCampaign.tsx
@@ -6,6 +6,7 @@ import { selectAllCampaign } from "@/core/store/features/campaigns/campaignSlice
 import { useAppSelector } from "@/core/hooks/storeHooks"
 
 import { ArrowRight, Clock } from "lucide-react"
+import { Button } from "../ui/button"
 
 export default function FeaturedCampaign() {
   const allCampaign = useAppSelector(selectAllCampaign)
@@ -108,12 +109,14 @@ export default function FeaturedCampaign() {
                   </div>
 
                   <div className="flex justify-between items-center mt-4">
-                    <Link
+                   <Button className="bg-blue-600 hover:bg-blue-700 ">
+                   <Link
                       href={`/campaign/${campaign._id}`}
-                      className="bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium py-2 px-4 rounded-lg transition-colors"
+                      className=" text-white text-sm font-medium  transition-colors"
                     >
                       Support Now
                     </Link>
+                   </Button>
                   </div>
                 </div>
               </div>
