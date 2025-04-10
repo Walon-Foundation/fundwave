6a6b794c36da5efc86f6826551e934617ba43582
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-04-10 00:37:27 +0000

    edit

--- src/app/(Auth)/verification/page.tsx
+++ src/app/(Auth)/verification/page.tsx
@@ -7,12 +7,12 @@ export default function Verification() {
     const router = useRouter();
 
     useEffect(() => {
-        const timer = setTimeout(() => {
+        setTimeout(() => {
             console.log("redirecting to login");
             router.push("/login");
         },5000)
         
-        return () => clearTimeout(timer);
+        // return () => clearTimeout(timer);
     }, [router])
   return (
     <div>account verified</div>
