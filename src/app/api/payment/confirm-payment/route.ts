b5fdb63174bcfe8a8e6e848bb7a918aef1e7fe15
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-06-23 20:01:09 +0000

    add monime route and the confirmpayment route for the webhook

--- src/app/api/payment/confirm-payment/route.ts
+++ src/app/api/payment/confirm-payment/route.ts
@@ -1,13 +1,14 @@
 import { NextRequest } from "next/server";
-import createPaymentCode from "@/core/helpers/generateCode";
 import { errorHandler } from "@/core/helpers/errorHandler";
 import { apiResponse } from "@/core/helpers/apiResponse";
 import { cookies } from "next/headers";
 import jwt  from "jsonwebtoken"
+import { ConfirmPayment } from "@/core/types/monimeType";
 
 
 export async function POST(req:NextRequest){
     try{
+
         const token = (await cookies()).get("accessToken")?.value as string
         if(!token){
             return errorHandler(401, "unauthorized", "no access token found")
@@ -17,26 +18,19 @@ export async function POST(req:NextRequest){
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET! as string) as {id:string, username:string, iscampaign:boolean};
         const decodedUser = decodedToken;
 
-        const body = await req.json()
+        const data = await req.json()
+        if(!data){
+            return errorHandler(400, "invalid webhook data", null)
+        }
 
-        const { phoneNumber, amount, campaignName } = body
+        const paymentData = data as ConfirmPayment
 
-        //validate later with zod
+        if(paymentData.status === "completed"){
+            console.log(`Payment for the user ${decodedUser.username} is compelete`)
+        }
 
-        const data = await createPaymentCode(campaignName, decodedUser.username, amount,phoneNumber, decodedUser.id)
-
-        //stored payment in the database
-
-        const code = data?.result.ussdCode
-
-        return apiResponse("payment code generate", 201, {
-            ussdCode:code
-        })
-
+        return apiResponse("completed", 200, null)
     }catch(err){
-        return errorHandler(500, "internal status error", err)
+        return errorHandler(500, "internal server error", err)
     }
 }
-
-
-
