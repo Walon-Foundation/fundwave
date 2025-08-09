6534ac5890e58668646e8761549c15cf4db0d563
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-08-09 00:16:21 +0000

    refactor: replace token-based authentication with Clerk's auth method in comments routes

--- app/api/campaigns/[id]/comments/[commentId]/route.ts
+++ app/api/campaigns/[id]/comments/[commentId]/route.ts
@@ -2,28 +2,19 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken"
 import { db } from "../../../../../../db/drizzle";
-import { commentTable } from "../../../../../../db/schema";
+import { commentTable, userTable } from "../../../../../../db/schema";
 import { and, eq } from "drizzle-orm";
+import { auth } from "@clerk/nextjs/server";
 
 
 export async function Delete(req:NextRequest, {params}:{params:Promise<{commentId:string}>}){
     try{
-        const token = (await cookies()).get("accessToken")?.value
-        if(!token){
-            return NextResponse.json({
-                error:"user not authenticated",
-            }, { status:401 })
-        }
-        const user = jwt.verify(token, process.env.ACCESS_TOKEN!) as { id:string } 
-        if(!user){
-            return NextResponse.json({
-                error:"invalid user auth token",
-            }, { status:401 })
-        }
+        const { userId } = await auth()
+        const userExist = await db.select().from(userTable).where(eq(userTable.clerkId, userId as string)).limit(1)
 
         const commentId = (await params).commentId
 
-        await db.delete(commentTable).where(and(eq(commentTable.id, commentId), eq(commentTable.userId, user.id)))
+        await db.delete(commentTable).where(and(eq(commentTable.id, commentId), eq(commentTable.userId, userExist[0].id)))
 
         return NextResponse.json({
             message:"comment deleted successfully",
--- app/api/campaigns/[id]/comments/route.ts
+++ app/api/campaigns/[id]/comments/route.ts
@@ -1,28 +1,19 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken"
 import { createComment } from "../../../../../validations/comment";
 import { commentTable, userTable } from "../../../../../db/schema";
 import { db } from "../../../../../db/drizzle";
 import { nanoid } from "nanoid";
 import { eq } from "drizzle-orm";
+import { auth } from "@clerk/nextjs/server";
 
 
 export async function POST(req:NextRequest, {params}:{params:Promise<{id:string}>}){
   try{
     //auth validation
-    const token = (await cookies()).get("accessToken")?.value
-    if(!token){
-      return NextResponse.json({
-        error:"user is not authenticated",
-      }, { status:401 })
-    }
+    const { userId } = await auth()
+    const userExist = await db.select().from(userTable).where(eq(userTable.clerkId, userId as string)).limit(1)
 
-    const user = jwt.verify(token, process.env.ACCESS_TOKEN!) as { id:string}
-
-    const userExist = await db.select().from(userTable).where(eq(userTable.id, user.id)).limit(1)
-
-    if(!user || userExist.length === 0){
+    if(!userId || userExist.length === 0){
       return NextResponse.json({
         error:"invalid auth token",
       }, { status:401 })
@@ -46,7 +37,7 @@ export async function POST(req:NextRequest, {params}:{params:Promise<{id:string}
       campaignId:id,
       message:data.comment,
       username:userExist[0].name,
-      userId:user.id
+      userId:userExist[0].id
     })
 
     return NextResponse.json({
