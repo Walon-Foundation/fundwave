245914dfa894ccc4b12f6810f4aeecf6c85c782c
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-08-14 11:44:35 +0000

    make a demo route

--- app/api/campaigns/[id]/payments/[paymentId]/route.ts
+++ app/api/campaigns/[id]/payments/[paymentId]/route.ts
@@ -1,13 +1,16 @@
-// import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 
-// export async function DELETE(req:NextRequest,{params}:{params:Promise<{paymentId:string}>}){
-//     try{
-        
-//     }catch(err){
-//         process.env.NODE_ENV === "development" ? console.log(err):""
-//         return NextResponse.json({
-//             error:"internal server error",
-//         }, {status:500})
-//     }
-// }
+export async function DELETE(req:NextRequest,{params}:{params:Promise<{paymentId:string}>}){
+    try{
+        return NextResponse.json({
+            ok:false,
+            message:"payment deleted successfully",
+        }, { status:204 })
+    }catch(err){
+        process.env.NODE_ENV === "development" ? console.log(err):""
+        return NextResponse.json({
+            error:"internal server error",
+        }, {status:500})
+    }
+}
