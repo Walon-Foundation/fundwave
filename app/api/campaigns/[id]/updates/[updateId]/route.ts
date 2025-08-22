1f7abe8f61b533578a9ceebbd7e5451fe58fd145
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-08-22 15:49:35 +0000

    did the following: - add the patch api route for update - add the fields for the edit for update

--- app/api/campaigns/[id]/updates/[updateId]/route.ts
+++ app/api/campaigns/[id]/updates/[updateId]/route.ts
@@ -3,6 +3,8 @@ import { db } from "../../../../../../db/drizzle";
 import { campaignTable, updateTable, userTable } from "../../../../../../db/schema";
 import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
+import { sendNotification } from "@/lib/notification";
+import { createUpdate } from "@/validations/update";
 
 
 export async function DELETE(req:NextRequest, {params}:{params:Promise<{updateId:string}>}){
@@ -49,6 +51,59 @@ export async function DELETE(req:NextRequest, {params}:{params:Promise<{updateId
         process.env.NODE_ENV === "development" ? console.log(err):""
         return NextResponse.json({
             error:"internal server error",
+        }, { status:500 })
+    }
+}
+
+
+
+export async function PATCH(req:NextRequest, {params}:{params:Promise<{updateId:string}>}){
+    try{
+        const { userId:clerkId } = await auth()
+        const user = (await db.select().from(userTable).where(eq(userTable.clerkId, clerkId!)).limit(1).execute())[0]
+
+        if(!user || !clerkId){
+            return NextResponse.json({
+                ok:false,
+                message:"user is not authenticated",
+            }, { status:401 })
+        }
+
+        const updateId = (await params).updateId
+
+        const reqBody = await req.formData()
+
+        const title = reqBody.get("title")
+        const content = reqBody.get("content")
+
+        const { success, data, error } = createUpdate.safeParse({
+            title,
+            content
+        })
+
+        if(!success){
+            process.env.NODE_ENV === "development" ? console.log(error.message):""
+            return NextResponse.json({
+                ok:false,
+                message:"invalid request body",
+            }, { status:400 })
+        }
+
+        const updateUpdate = (await db.update(updateTable).set({ title:data.title, message:data.content, updatedAt:new Date()}).where(eq(updateTable.id, updateId)).returning().execute())[0]
+
+        await sendNotification("update updated", "update", user.id, updateUpdate.campaignId)
+
+        return NextResponse.json({
+            ok:true,
+            message:"updated update",
+            data:updateUpdate
+        }, { status:200 })
+        
+    }catch(err){
+        process.env.NODE_ENV === "development" ? console.log(err):""
+        return NextResponse.json({
+            ok:false,
+            message:"internal server error",
         }, { status:500 })
     }
 }
