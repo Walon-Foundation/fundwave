a7dfae5e4cba39bb834760f0498212cd89fb5092
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-09-02 15:59:25 +0000

    made some change to the drizzle.ts file

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -8,6 +8,11 @@ neonConfig.webSocketConstructor = ws;
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 // neonConfig.poolQueryViaFetch = true
 
-const sql = neon(process.env.NODE_ENV === "development" ? process.env.DATABASE_URL_DEV! : process.env.DATABASE_URL_PROD!);
+const connectionString = process.env.NODE_ENV === "development"? process.env.DATABASE_URL_DEV : process.env.DATABASE_URL_PROD
+if(!connectionString){
+    throw new Error("no connection string found")
+}
+
+const sql = neon(connectionString);
 
 export const db = drizzle(sql, {schema});
