a07aea167ac5767d89776b7e785a5527ba1a4e07
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-09-05 00:06:59 +0000

    add the amount and provider to the withdraw validator

--- validations/withdrawal.ts
+++ validations/withdrawal.ts
@@ -2,5 +2,7 @@ import { z } from "zod";
 
 
 export const withdrawSchema = z.object({
-    phoneNumber: z.string().min(2, "Phone number is required")
+    phoneNumber: z.string().min(2, "Phone number is required"),
+    amount:z.number().min(1, "amount is required"),
+    provider:z.string().min(1, "proivder is required")
 })
