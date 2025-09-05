7113770941a7d1e9cbdd81f82e76737d72c19b39
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-09-05 00:37:47 +0000

    remove some of the old images

--- assets/assets.ts
+++ assets/assets.ts
@@ -1,39 +1,14 @@
-import logo from './logo.png';
-import holder from './holder.png'
-import g2_logo from './g2_logo.png'
-import f_logo from './f_logo.png'
-import bootcamp from './bootcamp.jpg'
-import knee  from './knee.jpg'
-import mobilmed from './mobilmed.jpg'
-import solar from './solar.jpg'
-import stadium from './stadium.jpg'
-import water from './water.jpg'
-import aprof from './aprof.jpg'
-import cancer from './cancer.jpg'
-import clean from './clean.jpg'
+
 import walon from './walon.jpg'
 import swaray from './swaray.jpg'
 import nema from './nema.jpg'
 import tjalloh from './tjalloh.jpg'
-import funding from './funding.png'
+import logo from "./logo.png"
 
 export {
-    logo,
-    holder,
-    g2_logo,
-    f_logo,
-    water,
-    stadium,
-    solar,
-    mobilmed,
-    knee,
-    bootcamp,
-    aprof,
-    cancer,
-    clean,
     walon,
     swaray,
     tjalloh,
     nema,
-    funding
+    logo
 }
